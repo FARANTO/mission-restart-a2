1b793fa52d695391bb2ef2c86a40a1456e3a0fc1
ANTO <faruquerahman03@gmail.com>
2026-03-09 02:00:26 +0600

    Third Commit

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,4 +1,6 @@
 import "./Banner.css";
+import  LeftVector  from "../banner/Vector.svg?react"; 
+import  RightVector  from "../banner/Vector 1.svg?react";
 
 function Banner() {
   return (
@@ -6,14 +8,23 @@ function Banner() {
       <div className="banner-container">
 
         <div className="stat-card progress">
+        <LeftVector className="vector left-vector" />
+        <div className="stat-content">
           <p className="stat-title">In-Progress</p>
           <h2 className="stat-number">0</h2>
+          </div>
+          <RightVector className="vector right-vector" />
         </div>
 
         <div className="stat-card resolved">
+         <LeftVector className="vector left-vector" /> 
+         <div className="stat-content">
           <p className="stat-title">Resolved</p>
           <h2 className="stat-number">0</h2>
         </div>
+        <RightVector className="vector right-vector" />
+        </div>
+
 
       </div>
     </div>
